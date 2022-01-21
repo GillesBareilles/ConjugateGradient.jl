e37330a383499deb066c6e820e43ee9cfcb6afe4
Gilles Bareilles <gilles.bareilles@protonmail.com>
2022-01-21 08:50:10 +0100

    change arguments to solve Ax = b (rather Ax+b = 0)

--- src/ConjugateGradient.jl
+++ src/ConjugateGradient.jl
@@ -4,24 +4,30 @@ using LinearAlgebra
 using DocStringExtensions
 using Printf
 
+
+function solve_tCG(A::Tm, b; ν=1e-3, ϵ_residual = 1e-13, maxiter=1e5, printlev=0) where {Tm <: AbstractMatrix}
+    A!(res, η) = (res .= A * η)
+    return solve_tCG(A!, b; ν, ϵ_residual, maxiter, printlev)
+end
+
 """
     $(SIGNATURES)
 
 Conjugate gradient method to solve the linear problem:
-    find d s.t. hessf_x_h(d) + gradf_k = 0
+    find d s.t. A * x = b
 
 Note that we are looking for a direction d in the tangent space of M at x, and the kernel of
 ∇²f_M contains at least the orthogonal to this set. CG should naturally give a solution
 living in the tangent space.
 """
-function solve_tCG(gradfₖ, hessf_h!; ν=1e-3, ϵ_residual = 1e-13, maxiter=1e5, printlev=0)
-    rⱼ, rⱼ_prev = similar(gradfₖ), similar(gradfₖ)
-    vⱼ, vⱼ_prev = similar(gradfₖ), similar(gradfₖ)
-    hessf_dₖ, hessf_vⱼ = similar(gradfₖ), similar(gradfₖ)
-    dₖ = similar(gradfₖ)
+function solve_tCG(A!::Function, b; ν=1e-3, ϵ_residual = 1e-13, maxiter=1e5, printlev=0)
+    rⱼ, rⱼ_prev = similar(b), similar(b)
+    vⱼ, vⱼ_prev = similar(b), similar(b)
+    Adₖ, Avⱼ = similar(b), similar(b)
+    dₖ = similar(b)
     dₖ .= 0
-    hessf_dₖ .= 0
-    hessf_vⱼ .= 0
+    Adₖ .= 0
+    Avⱼ .= 0
 
     d_type = :Unsolved
     j = 0
@@ -29,8 +35,8 @@ function solve_tCG(gradfₖ, hessf_h!; ν=1e-3, ϵ_residual = 1e-13, maxiter=1e5
     (printlev>0) && @printf "\n    j norm(rⱼ)            norm(vⱼ)             ⟨vⱼ, hessf[vⱼ]⟩   ν * norm(vⱼ)^2\n"
     while true
         # current residual, conjugated direction
-        hessf_h!(hessf_dₖ, dₖ)
-        rⱼ .= hessf_dₖ .+ gradfₖ
+        A!(Adₖ, dₖ)
+        rⱼ .= Adₖ .- b
 
         vⱼ = - rⱼ
         if j ≥ 1
@@ -38,12 +44,12 @@ function solve_tCG(gradfₖ, hessf_h!; ν=1e-3, ϵ_residual = 1e-13, maxiter=1e5
             vⱼ += βⱼ * vⱼ_prev
         end
 
-        hessf_h!(hessf_vⱼ, vⱼ)
-        (printlev>0) && @printf "%5i %.10e    %.10e    % .10e     %.10e\n" j norm(rⱼ) norm(vⱼ) dot(vⱼ, hessf_vⱼ) ν * norm(vⱼ)^2
-        if norm(rⱼ) < ϵ_residual || dot(vⱼ, hessf_vⱼ) < ν * norm(vⱼ)^2 || j > maxiter
+        A!(Avⱼ, vⱼ)
+        (printlev>0) && @printf "%5i %.10e    %.10e    % .10e     %.10e\n" j norm(rⱼ) norm(vⱼ) dot(vⱼ, Avⱼ) ν * norm(vⱼ)^2
+        if norm(rⱼ) < ϵ_residual || dot(vⱼ, Avⱼ) < ν * norm(vⱼ)^2 || j > maxiter
             ## Satisfying point obtained
             if j == 0
-                dₖ = -gradfₖ
+                dₖ .= b
             end
 
             if norm(rⱼ) < ϵ_residual
@@ -54,7 +60,7 @@ function solve_tCG(gradfₖ, hessf_h!; ν=1e-3, ϵ_residual = 1e-13, maxiter=1e5
                 @debug "Exiting: j > maxiter : $j > $maxiter\n"
             else
                 d_type = :QuasiNegCurvature
-                a = dot(vⱼ, hessf_vⱼ)
+                a = dot(vⱼ, Avⱼ)
                 b = ν * norm(vⱼ)^2
                 @debug "Exiting: ⟨vⱼ, hessf_x(vⱼ)⟩ < ν * ||vⱼ||² : $a < $b\n"
             end
@@ -62,7 +68,7 @@ function solve_tCG(gradfₖ, hessf_h!; ν=1e-3, ϵ_residual = 1e-13, maxiter=1e5
             break
         end
 
-        tⱼ = - dot(rⱼ, vⱼ) / dot(vⱼ, hessf_vⱼ)
+        tⱼ = - dot(rⱼ, vⱼ) / dot(vⱼ, Avⱼ)
         dₖ += tⱼ * vⱼ
 
         rⱼ_prev = deepcopy(rⱼ)
